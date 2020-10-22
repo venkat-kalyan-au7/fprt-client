cda936ab7adffa0e6d8fc567325f322bbaa421a5
Venkat Kalyan <60262281+venkat-kalyan-au7@users.noreply.github.com>
2020-10-22 09:41:49 +0530

    client

---
+++ src/components/TrelloBoard/Members.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { addMember } from '../../Redux/actions/board';
+import getInitials from '../../Helpers/getInitials';
+import { TextField, Button } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import Tooltip from '@material-ui/core/Tooltip';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CloseIcon from '@material-ui/icons/Close';
+
+const Members = () => {
+  const [inviting, setInviting] = useState(false);
+  const [user, setUser] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [users, setUsers] = useState([]);
+  const boardMembers = useSelector((state) => state.board.board.members);
+  const searchOptions = users.filter((user) =>
+    boardMembers.find((boardMember) => boardMember.user === user._id) ? false : true
+  );
+  const dispatch = useDispatch();
+
+  const handleInputValue = async (newInputValue) => {
+    setInputValue(newInputValue);
+    if (newInputValue && newInputValue !== '') {
+      const search = (await axios.get(`/api/users/${newInputValue}`)).data.slice(0, 5);
+      setUsers(search && search.length > 0 ? search : []);
+    }
+  };
+
+  const onSubmit = async () => {
+    dispatch(addMember(user._id));
+    setUser(null);
+    setInputValue('');
+    setInviting(false);
+  };
+
+  return (
+    <div className='board-members-wrapper'>
+      <div className='board-members'>
+        {boardMembers.map((member) => {
+          return (
+            <Tooltip title={member.name} key={member.user}>
+              <Avatar className='avatar'>{getInitials(member.name)}</Avatar>
+            </Tooltip>
+          );
+        })}
+      </div>
+ 
+    </div>
+  );
+};
+
+export default Members;
---
+++ src/pages/Board.js
@@ -0,0 +1,81 @@
+import React, { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { getBoard, moveCard, moveList } from '../Redux/actions/board';
+import { CircularProgress, Box } from '@material-ui/core';
+import BoardTitle from '../components/TrelloBoard/BoardTitle';
+
+import List from '../components/StatusList/List';
+import CreateList from '../components/TrelloBoard/CreateList';
+import Members from '../components/TrelloBoard/Members';
+import Navbar from '../components/Common/Navbar';
+
+const Board = ({ match }) => {
+  const board = useSelector((state) => state.board.board);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBoard(match.params.id));
+  }, [dispatch, match.params.id]);
+
+  if (!isAuthenticated) {
+    return <Redirect to='/' />;
+  }
+
+  const onDragEnd = (result) => {
+    const { source, destination, draggableId, type } = result;
+    if (!destination) {
+      return;
+    }
+    if (type === 'card') {
+      dispatch(
+        moveCard(draggableId, {
+          fromId: source.droppableId,
+          toId: destination.droppableId,
+          toIndex: destination.index,
+        })
+      );
+    } else {
+      dispatch(moveList(draggableId, { toIndex: destination.index }));
+    }
+  };
+
+  return !board ? (
+    <Fragment>
+      <Navbar />
+      <Box className='board-loading'>
+        <CircularProgress />
+      </Box>
+    </Fragment>
+  ) : (
+    <div className='board-and-navbar'>
+      <Navbar />
+      <section className='board'>
+        <div className='board-top'>
+          <div className='board-top-left'>
+            <BoardTitle board={board} />
+            <Members />
+          </div>
+         
+        </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId='all-lists' direction='horizontal' type='list'>
+            {(provided) => (
+              <div className='lists' ref={provided.innerRef} {...provided.droppableProps}>
+                {board.lists.map((listId, index) => (
+                  <List key={listId} listId={listId} index={index} />
+                ))}
+                {provided.placeholder}
+                <CreateList />
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </section>
+    </div>
+  );
+};
+
+export default Board;
